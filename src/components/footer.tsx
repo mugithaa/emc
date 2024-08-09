c3beee4ea4a710516553672e704f5b6fe46bb741
mugithaa <karthik.d@aol.com>
2024-08-10 00:33:34 +0530

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -22,14 +22,14 @@ export function Footer() {
           <Container.Inner>
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
               <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
-                <NavLink href="/"><EMC</NavLink>
+                <NavLink href="/">EMC</NavLink>
                 <NavLink href="/articles">Articles</NavLink>
                 <NavLink href="/projects">Projects</NavLink>
                 <NavLink href="/uses">Uses</NavLink>
                 <NavLink href="/about">About</NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
-                &copy; {new Date().getFullYear()} {EMC}. All rights
+                &copy; {new Date().getFullYear()} EMC. All rights
                 reserved.
               </p>
             </div>
